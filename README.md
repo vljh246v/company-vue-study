bf8485d7672924076960c424bfefd3cadc852612
jaehyun <vljh246v@naver.com>
2020-06-02 00:32:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# company-vue-study
---
+++ quiz/03-3/answer/app.js
@@ -0,0 +1,20 @@
+Vue.component('child-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>'
+})
+
+
+Vue.component('sibling-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>'
+})
+
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue! passed from Parent Component',
+        anotherMessage: 'Is this a child component?'
+    }
+})
